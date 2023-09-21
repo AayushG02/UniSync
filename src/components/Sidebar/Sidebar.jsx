db87c7dc4a806b49a3de1cbf2543eea59d37d9db
AayushG02 <aayush.girdhar2@gmail.com>
2023-09-21 12:59:33 +0530

    optimised sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import HomeIcon from "@mui/icons-material/Home";
-import FolderIcon from "@mui/icons-material/Folder";
 import FolderCopyIcon from "@mui/icons-material/FolderCopy";
 import ChatIcon from "@mui/icons-material/Chat";
 import AssessmentIcon from "@mui/icons-material/Assessment";
@@ -8,26 +8,72 @@ import "./Sidebar.css";
 
 const Sidebar = () => {
   const [sidebarVisible, setSidebarVisible] = useState(true);
+
   return (
-    <div className="sidebar-container">
+    <div
+      className={`sidebar-container ${
+        sidebarVisible ? "expanded" : "collapsed"
+      } `}
+    >
+      <div
+        className="sidebar-collapse"
+        onClick={() => setSidebarVisible(!sidebarVisible)}
+      >
+        <ArrowForwardIosIcon
+          className="collapse-icon"
+          style={
+            sidebarVisible
+              ? {
+                  transform: "rotate(-180deg)",
+                  transition: "all 150ms ease-in-out",
+                }
+              : {
+                  transform: "rotate(0)",
+                  transition: "all 150ms ease-in-out",
+                }
+          }
+        />
+      </div>
+
       <img src="" alt="" className="sidebar-logo" />
-      <div className="sidebar-item">
-        <HomeIcon className="sidebar-icon" /> Home
+      <div
+        className={`sidebar-item ${
+          sidebarVisible ? "item-expand" : "item-collapse"
+        } `}
+      >
+        <HomeIcon className="sidebar-icon" />
+        {sidebarVisible && <span>Home</span>}
       </div>
-      <div className="sidebar-item">
-        <FolderIcon className="sidebar-icon" />
-        My Projects
+      <div
+        className={`sidebar-item ${
+          sidebarVisible ? "item-expand" : "item-collapse"
+        } `}
+      >
+        <FolderCopyIcon className="sidebar-icon" />
+        {sidebarVisible && <span>My Projects</span>}
       </div>
-      <div className="sidebar-item">
+      <div
+        className={`sidebar-item ${
+          sidebarVisible ? "item-expand" : "item-collapse"
+        } `}
+      >
         <ChatIcon className="sidebar-icon" />
-        Chat
+        {sidebarVisible && <span>Chat</span>}
       </div>
-      <div className="sidebar-item">
+      <div
+        className={`sidebar-item ${
+          sidebarVisible ? "item-expand" : "item-collapse"
+        } `}
+      >
         <AssessmentIcon className="sidebar-icon" />
-        Analytics
+        {sidebarVisible && <span>Analytics</span>}
       </div>
-      <div className="sidebar-item">
-        <button className="logout-btn">Logout</button>
+      <div
+        className={`sidebar-item ${
+          sidebarVisible ? "item-expand" : "item-collapse"
+        } `}
+      >
+        {sidebarVisible && <button className="logout-btn">Logout</button>}
       </div>
     </div>
   );
