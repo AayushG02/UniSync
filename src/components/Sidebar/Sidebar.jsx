74f3d8f8dd430baa35104d31888addad67411bd2
AayushG02 <aayush.girdhar2@gmail.com>
2023-09-22 22:03:02 +0530

    finalised sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,105 +1,68 @@
-import React, { useState, useEffect } from "react";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
 import HomeIcon from "@mui/icons-material/Home";
-import FolderCopyIcon from "@mui/icons-material/FolderCopy";
 import ChatIcon from "@mui/icons-material/Chat";
 import AssessmentIcon from "@mui/icons-material/Assessment";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import LogoutIcon from "@mui/icons-material/Logout";
 import "./Sidebar.css";
 
 const Sidebar = () => {
   // const [sidebarVisible, setSidebarVisible] = useState(true);
+  const pathname = useLocation().pathname;
 
   return (
     <div className="sidebar-container">
       <div className="sidebar-items">
-        <img src="" alt="" className="sidebar-logo" />
-        <div className="sidebar-item">
+        <Link to="/" classname="link">
+          <div className="sidebar-logo">
+            <img src={logo}/>
+            <span className="item-text">UniSync</span>
+          </div>
+        </Link>
+        <Link
+          to="/"
+          className={`sidebar-item ${
+            pathname === "/" ? "sidebar-item-selected" : ""
+          } `}
+        >
           <HomeIcon fontSize="large" /> <span className="item-text">Home</span>
-        </div>
-        <div className="sidebar-item">
-          <FolderCopyIcon fontSize="large" />
-          <span className="item-text">Projects</span>
-        </div>
-        <div className="sidebar-item">
-          <ChatIcon fontSize="large" /> <span className="item-text">Chat</span>
-        </div>
-        <div className="sidebar-item">
+        </Link>
+        <Link
+          to="/messages"
+          className={`sidebar-item ${
+            pathname === "/messages" ? "sidebar-item-selected" : ""
+          } `}
+        >
+          <ChatIcon fontSize="large" />
+          <span className="item-text">Messages</span>
+        </Link>
+        <Link
+          to="/analytics"
+          className={`sidebar-item ${
+            pathname === "/analytics" ? "sidebar-item-selected" : ""
+          } `}
+        >
           <AssessmentIcon fontSize="large" />
           <span className="item-text">Analytics</span>
+        </Link>
+        <Link
+          to="/profile"
+          className={`sidebar-item ${
+            pathname === "/profile" ? "sidebar-item-selected" : ""
+          } `}
+        >
+          <AccountBoxIcon fontSize="large" />
+          <span className="item-text">Profile</span>
+        </Link>
+        <div className="sidebar-item">
+          <LogoutIcon fontSize="large" />
+          <span className="item-text">Logout</span>
         </div>
-        <div className="sidebar-item">Logout</div>
       </div>
     </div>
   );
 };
-
-//   <div
-//     className={`sidebar-container ${
-//       sidebarVisible ? "expanded" : "collapsed"
-//     } `}
-//   >
-//     <div
-//       className="sidebar-collapse"
-//       onClick={() => setSidebarVisible(!sidebarVisible)}
-//     >
-//       <ArrowForwardIosIcon
-//         className="collapse-icon"
-//         style={
-//           sidebarVisible
-//             ? {
-//                 transform: "rotate(-180deg)",
-//                 transition: "all 150ms ease-in-out",
-//               }
-//             : {
-//                 transform: "rotate(0)",
-//                 transition: "all 150ms ease-in-out",
-//               }
-//         }
-//       />
-//     </div>
-
-//     <img src="" alt="" className="sidebar-logo" />
-//     <div
-//       className={`sidebar-item ${
-//         sidebarVisible ? "item-expand" : "item-collapse"
-//       } `}
-//     >
-//       <HomeIcon className="sidebar-icon" />
-//       {<span>Home</span>}
-//     </div>
-//     <div
-//       className={`sidebar-item ${
-//         sidebarVisible ? "item-expand" : "item-collapse"
-//       } `}
-//     >
-//       <FolderCopyIcon className="sidebar-icon" />
-//       {<span>My Projects</span>}
-//     </div>
-//     <div
-//       className={`sidebar-item ${
-//         sidebarVisible ? "item-expand" : "item-collapse"
-//       } `}
-//     >
-//       <ChatIcon className="sidebar-icon" />
-//       {<span>Chat</span>}
-//     </div>
-//     <div
-//       className={`sidebar-item ${
-//         sidebarVisible ? "item-expand" : "item-collapse"
-//       } `}
-//     >
-//       <AssessmentIcon className="sidebar-icon" />
-//       {<span>Analytics</span>}
-//     </div>
-//     <div
-//       className={`sidebar-item ${
-//         sidebarVisible ? "item-expand" : "item-collapse"
-//       } `}
-//     >
-//       {sidebarVisible && <button className="logout-btn">Logout</button>}
-//     </div>
-//   </div>
-// );
-// };
 
 export default Sidebar;
